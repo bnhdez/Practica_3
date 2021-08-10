9953671510d44231d63045f29780e365166f56ee
Tecnolab <borisnandez93@gmail.com>
2021-08-10 08:36:03 -0600

    cambios

---
+++ Practica 3/Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1, num, prod = 1;
+            
+            Console.WriteLine("Ingrese un numero del 1 al 10");
+            num = int.Parse(Console.ReadLine());
+
+            while (contador <= 10)
+            {
+                prod = num * prod;
+
+                Console.WriteLine("La multiplicacion es "+ num +"x"+ contador, "=", prod);
+
+                contador = contador + 1;
+            }
+
+            
+        }
+    }
+}
