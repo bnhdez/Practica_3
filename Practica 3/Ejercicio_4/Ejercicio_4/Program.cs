175329f18231046e15ce5ecfc26b1d2e9c136916
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-08-15 16:36:36 -0600

    cambios

--- Practica 3/Ejercicio_4/Ejercicio_4/Program.cs
+++ Practica 3/Ejercicio_4/Ejercicio_4/Program.cs
@@ -6,27 +6,31 @@ namespace Ejercicio_4
     {
         static void Main(string[] args)
         {
-            double muestra;
+            double muestra, promedio, total = 0;
             int cont = 1;
 
             Console.WriteLine("***********Ingrese las 25 muestras recibidas en una hora***********");
-            while (cont <= 5)
+            //este ciclo while sirve para tomar los datos de todas las muestras
+            while (cont <= 25)
             {
                 Console.WriteLine("Muestra " + cont);
                 muestra = double.Parse(Console.ReadLine());
+                //total sera la suma de las muestras
+                total = total + muestra;
 
+                //condicion if para establecer los parametros de control
                 if ((muestra >= 0.5977) && (muestra <= 0.8239))
                 {
                     Console.WriteLine("Bajo Control!");
                 }
                 else 
                 {
-                    Console.WriteLine("No esta dentro de los parametros de control");
+                    Console.WriteLine("Fuera de control!");
                 }
-
                 cont = cont + 1;
             }
-
+            promedio = total / 25;
+            Console.WriteLine("El promedio de las muestras es de: " + promedio);
         }
     }
 }
