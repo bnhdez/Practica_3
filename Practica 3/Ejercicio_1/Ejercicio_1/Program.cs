7b2c08ea04b38c30f8a2b14f414673b39a068296
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-08-15 15:44:29 -0600

    cambios

--- Practica 3/Ejercicio_1/Ejercicio_1/Program.cs
+++ Practica 3/Ejercicio_1/Ejercicio_1/Program.cs
@@ -10,9 +10,11 @@ namespace Ejercicio_1
             
             Console.WriteLine("Ingrese un numero del 1 al 10");
             num = int.Parse(Console.ReadLine());
-
+            
+            //mientras el contador sea menor a 10, el ciclo se repite
             while (contador <= 10)
             {
+                //el producto es el resultado de multiplicar el numero ingresado por el contador que aumenta 1 cada vez
                 prod = num * contador;
 
                 Console.WriteLine("La multiplicacion es "+ num +"x"+ contador+ "="+ prod);
