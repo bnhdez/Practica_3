c7fdd2d9a822b3a457d02b95bf5af640cdd35768
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-08-15 16:07:35 -0600

    cambios

---
+++ Practica 3/Ejercicio_4/Ejercicio_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double muestra;
+            int cont = 1;
+
+            Console.WriteLine("***********Ingrese las 25 muestras recibidas en una hora***********");
+            while (cont <= 5)
+            {
+                Console.WriteLine("Muestra " + cont);
+                muestra = double.Parse(Console.ReadLine());
+
+                if ((muestra >= 0.5977) && (muestra <= 0.8239))
+                {
+                    Console.WriteLine("Bajo Control!");
+                }
+                else 
+                {
+                    Console.WriteLine("No esta dentro de los parametros de control");
+                }
+
+                cont = cont + 1;
+            }
+
+        }
+    }
+}
